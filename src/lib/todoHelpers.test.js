72a594fa05df3ac36329b79d29778820979fe37d
basementcz <you@example.com>
2017-02-04 22:31:20 +0100

    filter data on property values

--- src/lib/todoHelpers.test.js
+++ src/lib/todoHelpers.test.js
@@ -1,4 +1,4 @@
-import {addTodo, findById, toggleTodo, updateTodo, removeTodo} from './todoHelpers'
+import {addTodo, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './todoHelpers'
 
 test('addTodo should add the passed todo to the client', () => {
     const startTodos = [
@@ -136,4 +136,49 @@ test('removeTodo should not mutate the original array', () => {
     const result = removeTodo(startTodos, targetId)
 
     expect(result).not.toBe(startTodos)
+})
+
+test('filterTodoa should return all items for the root route', () => {
+    const startTodos = [
+        {id: 1, name: 'one', isComplete: false},
+        {id: 2, name: 'two', isComplete: true},
+        {id: 3, name: 'three', isComplete: false}
+    ]
+
+    const result = filterTodos(startTodos, '/')
+
+    expect(result).toEqual(startTodos)
+})
+
+test('filterTodoa should return only completed items for the complete route', () => {
+    const startTodos = [
+        {id: 1, name: 'one', isComplete: false},
+        {id: 2, name: 'two', isComplete: true},
+        {id: 3, name: 'three', isComplete: false}
+    ]
+
+    const expected = [
+        {id: 2, name: 'two', isComplete: true}
+    ]
+
+    const result = filterTodos(startTodos, '/complete')
+
+    expect(result).toEqual(expected)
+})
+
+test('filterTodoa should return only incomplited items for the active route', () => {
+    const startTodos = [
+        {id: 1, name: 'one', isComplete: false},
+        {id: 2, name: 'two', isComplete: true},
+        {id: 3, name: 'three', isComplete: false}
+    ]
+
+    const expected = [
+        {id: 1, name: 'one', isComplete: false},
+        {id: 3, name: 'three', isComplete: false}
+    ]
+
+    const result = filterTodos(startTodos, '/active')
+
+    expect(result).toEqual(expected)
 })
